4026872606caec9a3d7102acb99ee7fc75f54058
Katya <pomogisebesry@gmail.com>
2024-02-06 16:02:00 +1000

    06.02.2024

--- qwerty.ts
+++ qwerty.ts
@@ -30,20 +30,51 @@
 // 	}
 // }
 
-const b: string = 'qwerty'
-const a: number = 9
+// const b: string = 'qwerty'
+// const a: number = 9
 
-function showA(i: number) {
-	if (i < 1) {
-		// console.log(a)
-		// console.log(typeof a)
-		return 'a'
-	} else {
-		// console.log(b)
-		// console.log(typeof b)
-		return 'b'
-	}
+// function showA(i: number) {
+// 	if (i < 1) {
+// 		// console.log(a)
+// 		// console.log(typeof a)
+// 		return 'a'
+// 	} else {
+// 		// console.log(b)
+// 		// console.log(typeof b)
+// 		return 'b'
+// 	}
+// }
+// showA(6)
+
+// const ShowFunc: () => string = showA
+// class Katya {}
+
+// class Point {
+// 	x: number;
+// 	y: number;
+// }
+// function func() {
+// let point: Point;
+// }
+// let point: Point = new Point();
+// function move(){
+// 	point.x = 1;
+// 	point.y = 2;
+// }
+class Point3D {
+	x: number
+	y: number
+	z: number
 }
-showA(6)
 
-const ShowFunc: () => string = showA
+function func3d() {
+	let point3D: Point3D
+}
+
+let point3D: Point3D = new Point3D()
+
+function move3D() {
+	point3D.x = 1
+	point3D.y = 2
+	point3D.z = 4
+}
