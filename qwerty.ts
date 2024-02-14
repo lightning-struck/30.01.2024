c7a2c2013877646dc1a7230600c60708ffc967ce
Katya <pomogisebesry@gmail.com>
2024-02-14 11:39:38 +1000

    main

--- qwerty.ts
+++ qwerty.ts
@@ -61,20 +61,51 @@
 // 	point.x = 1;
 // 	point.y = 2;
 // }
-class Point3D {
+// class Point3D {
+// 	x: number
+// 	y: number
+// 	z: number
+// }
+
+// function func3d() {
+// 	let point3D: Point3D
+// }
+
+// let point3D: Point3D = new Point3D()
+
+// function move3D() {
+// 	point3D.x = 1
+// 	point3D.y = 2
+// 	point3D.z = 4
+// }
+class Point {
 	x: number
 	y: number
-	z: number
+	constructor(val_1?: number, val_2?: number) {
+		this.x = val_1
+		this.y = val_2
+	}
 }
 
-function func3d() {
-	let point3D: Point3D
+class Triangle {
+	private x1: Point
+	private y1: Point
+	private z1: Point
+
+	constructor(
+		x1?: number,
+		y1?: number,
+		x2?: number,
+		y2?: number,
+		x3?: number,
+		y3?: number
+	) {
+		this.x1 = new Point(x1, y1)
+		this.y1 = new Point(x2, y2)
+		this.z1 = new Point(x3, y3)
+	}
 }
 
-let point3D: Point3D = new Point3D()
+let katya = new Triangle(1, 23, 4, 56, 7, 3)
 
-function move3D() {
-	point3D.x = 1
-	point3D.y = 2
-	point3D.z = 4
-}
+// let point: Point = new Point()
